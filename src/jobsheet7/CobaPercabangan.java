0933c79f0cffde2b275b6d3fa7699952dd9210da
ASUS <ASUS@DESKTOP-OJM6A95>
2018-08-01 09:22:55 +0700

    Initial Commit

---
+++ src/jobsheet7/CobaPercabangan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet7;
+import java.util.Scanner;
+/**
+ *
+ * @author ASUS
+ */
+public class CobaPercabangan {
+    public static void main(String []args){
+        String identitas = "M.Sultan Sahara/ XRPL5 / 27 ";
+        System.out.println("Identitas : " + identitas);
+        
+        Scanner scanner = new Scanner (System.in);
+        System.out.print("Nilai a ? ");
+        int a = scanner.nextInt();
+    }
+}
