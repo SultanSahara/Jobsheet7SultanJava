d0ba83bfa1682ded10874912780b39ed0958c6e0
ASUS <ASUS@DESKTOP-OJM6A95>
2018-08-03 10:44:04 +0700

    Coba IF

--- src/jobsheet7/CobaPercabangan.java
+++ src/jobsheet7/CobaPercabangan.java
@@ -17,5 +17,22 @@ public class CobaPercabangan {
         Scanner scanner = new Scanner (System.in);
         System.out.print("Nilai a ? ");
         int a = scanner.nextInt();
-    }
+        
+        System.out.println("\nIF.. 1");
+        if(a < 5) System.out.println("nilai a kurang dari 5");
+        
+        System.out.println("\nIF.. 2");
+        if(a == 5)
+            System.out.println("nilai a sama dengan 5");
+        
+System.out.println("\nIF.. 3");
+if (a > 5){
+    System.out.println("isi Variable a : " + a);
+    System.out.println("nilai a lebih dari 5");
 }
+}
+}
+            
+    
+
+
