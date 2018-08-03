c79228d56b9249d97b5aa2be8fc401efcf3edf9f
ASUS <ASUS@DESKTOP-OJM6A95>
2018-08-03 12:49:38 +0700

    Tambah Input Bahan 2.

--- src/jobsheet7/saranbahan2resep.java
+++ src/jobsheet7/saranbahan2resep.java
@@ -22,5 +22,26 @@ public class saranbahan2resep {
     System.out.println("2. Telur");
     System.out.print("Masukkan no pilihan anda :");
     int bahan1 = scanner.nextInt();
-}
-}
+    
+     if (bahan1 == 1 || bahan1 == 2)
+    {
+        System.out.println("Bahan Kedua : ");
+        if(bahan1 == 1){
+            System.out.println("1. Susu");
+            System.out.println("2. Minyak Goreng");
+            System.out.println("3. Tidak ada");
+        }else{
+            System.out.println("1. Minyak Goreng");
+            System.out.println("2. Roti");
+            System.out.println("3. Tidak ada");
+        }
+        System.out.println("Masukkan no pilihan anda :");
+        int bahan2 = scanner.nextInt();
+        
+    }else
+         System.out.println("Mohon Maaf,pilihan tidak ditemukan, " + "tidak dapat memberikan saran resep");
+        }
+    }
+            
+
+
